8cfaa4714b8f84456062ece40469595f4f3d5ee2
sito <adrianologu123@gmail.com>
2025-11-21 14:10:02 +0100

    Genera una lista de 352 pasajeros distribuidos por categorías (niños, adultos, ancianos y tripulación).
    Inicializa un conjunto de balsas con distintos tiempos de rescate.
    
    Crea y ejecuta hilos que representan el proceso de rescate asociado a cada balsa.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.*;
+
+// Clase Main
 public class Main {
     public static void main(String[] args) {
+        // Crear pasajeros
+        List<Pasajero> pasajeros = new ArrayList<>();
+        int id = 1;
 
-        //Completar Codigos
+        // Ejemplo de distribución
+        for (int i = 0; i < 100; i++) pasajeros.add(new Pasajero(id++,
+                1)); // Niños
+        for (int i = 0; i < 100; i++) pasajeros.add(new Pasajero(id++,
+                2)); // Adultos
+        for (int i = 0; i < 100; i++) pasajeros.add(new Pasajero(id++,
+                3)); // Ancianos
+        for (int i = 0; i < 52; i++) pasajeros.add(new Pasajero(id++,
+                4)); // Tripulación
 
+        Barco barco = new Barco(pasajeros);
 
+        // Crear balsas
+        Balsa[] balsas = {
+                new Balsa("Acasta", 1, 0.5),
+                new Balsa("Banff", 2, 1),
+                new Balsa("Cadiz", 3, 2),
+                new Balsa("Deimos", 4, 4),
+                new Balsa("Expedición", 5, 8)
+        };
+
+        // Crear y arrancar hilos
+        for (Balsa b : balsas) {
+            new Thread(new Rescate(b, barco)).start();
+        }
     }
 }
