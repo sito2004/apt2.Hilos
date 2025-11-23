282483546c1398c06273b3545782f62fca9f414c
sito <adrianologu123@cloud.com>
2025-11-23 12:26:39 +0100

    implementar lista de pasajeros en el Barco método sacarPrioritario() para extraer pasajeros según prioridad

--- src/Barco.java
+++ src/Barco.java
@@ -2,15 +2,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Barco {
-
-    private final List<Pasajero> pasajeros = new ArrayList<>();
+    private final List<Pasajero> pasajeros;
 
     public Barco(List<Pasajero> pasajeros) {
-        this.pasajeros.addAll(pasajeros);
+        this.pasajeros = new ArrayList<>(pasajeros);
     }
 
-    public boolean hayPasajeros(){
+    public synchronized boolean hayPasajeros() {
         return !pasajeros.isEmpty();
     }
 
+    public synchronized Pasajero sacarPrioritario() {
+        if (pasajeros.isEmpty()) {
+            return null;
+        }
+
+        Pasajero prioritario = pasajeros.get(0);
+        for (Pasajero p : pasajeros) {
+            if (p.getPrioridad() < prioritario.getPrioridad()) {
+                prioritario = p;
+            }
+            if (prioritario.getPrioridad() == 1) {
+                break;
+            }
+        }
+
+        pasajeros.remove(prioritario);
+        return prioritario;
+    }
+
+    public synchronized int getNumPasajeros() {
+        return pasajeros.size();
+    }
 }
