d12f18328b875dd19ee320b3851b9e891e70952c
sito <adrianologu123@cloud.com>
2025-12-01 17:48:47 +0100

    He añadido un semáforo para controlar el acceso a la lista de pasajeros.
    He quitado synchronized del método hayPasajeros y he hecho que use el semáforo.
    
    He quitado synchronized del método sacarPrioritario y he hecho que use el semáforo.
    
    He quitado synchronized del método getNumPasajeros y he hecho que use el semáforo.

--- src/Barco.java
+++ src/Barco.java
@@ -1,18 +1,21 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Semaphore;
 
 public class Barco {
     private final List<Pasajero> pasajeros;
-
+    private final Semaphore semBarco = new Semaphore(1);
     public Barco(List<Pasajero> pasajeros) {
         this.pasajeros = new ArrayList<>(pasajeros);
     }
 
-    public synchronized boolean hayPasajeros() {
+    public synchronized boolean hayPasajeros() throws InterruptedException {
+        semBarco.acquire();
         return !pasajeros.isEmpty();
     }
 
-    public synchronized Pasajero sacarPrioritario() {
+    public synchronized Pasajero sacarPrioritario() throws InterruptedException {
+        semBarco.acquire();
         if (pasajeros.isEmpty()) {
             return null;
         }
@@ -22,16 +25,21 @@ public class Barco {
             if (p.getPrioridad() < prioritario.getPrioridad()) {
                 prioritario = p;
             }
-            if (prioritario.getPrioridad() == 1) {
-                break;
-            }
         }
 
         pasajeros.remove(prioritario);
         return prioritario;
     }
 
-    public synchronized int getNumPasajeros() {
-        return pasajeros.size();
+    public synchronized int getNumPasajeros() throws InterruptedException {
+        try {
+            semBarco.acquire();
+            return pasajeros.size();
+        }catch (InterruptedException e){
+            return -1;
+        }finally {
+            semBarco.release();
+        }
+
     }
 }
